d95580f0a1ae810893a936cc7575ffe97b71f281
sowro <sowrovnil59@gmail.com>
2025-01-03 00:43:10 +0600

    navagationview and  menu bar add

--- app/src/main/java/com/example/nav_25_2/MainActivity.kt
+++ app/src/main/java/com/example/nav_25_2/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.nav_25_2
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -30,21 +33,41 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
 
-  val navController=findNavController(R.id.Fragment_container_view)
+        val navController = findNavController(R.id.Fragment_container_view)
 
-        appBarConfiguration= AppBarConfiguration(
-                   setOf( R.id.blankFragment,
-                          R.id.blankFragment2,
-                          R.id.blankFragment3
-                        ),binding.drawerlayout
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.blankFragment,
+                R.id.blankFragment2,
+                R.id.blankFragment3
+            ), binding.drawerlayout
         )
 
-        setupActionBarWithNavController(navController,appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navagationView.setupWithNavController(navController)
 
 
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+
+     menuInflater.inflate(R.menu.main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            // Add other cases for other menu items if needed
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+
 
     override fun onSupportNavigateUp(): Boolean {
 
